7bbab557c840e6976e3efcf188f1eee637ec90ab
Chechu <22799163+ChechuDEV@users.noreply.github.com>
2024-08-31 13:38:03 +0200

    feat: add constants

---
+++ src/main/kotlin/de/infinityprojects/mcserver/utils/Constants.kt
@@ -0,0 +1,5 @@
+package de.infinityprojects.mcserver.utils
+
+const val SERVER_BRAND = "Melius"
+const val CPU_BENCHMARK_THREAD_NAME = "Melius-CPUBenchmark"
+const val CHUNK_SAVING_THREAD_NAME = "Melius-ChunkSaving"
