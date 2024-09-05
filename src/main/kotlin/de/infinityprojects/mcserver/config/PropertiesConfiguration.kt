7ac3d9eb46b253035b31e96b1b5f7bf526c47e19
Chechu <22799163+ChechuDEV@users.noreply.github.com>
2024-09-06 01:12:35 +0200

    feat: move methods to common configuration and file configuration

--- src/main/kotlin/de/infinityprojects/mcserver/config/PropertiesConfiguration.kt
+++ src/main/kotlin/de/infinityprojects/mcserver/config/PropertiesConfiguration.kt
@@ -1,102 +1,52 @@
 package de.infinityprojects.mcserver.config
 
 import java.io.File
+import java.io.InputStream
 import java.util.Properties
 
 class PropertiesConfiguration(
-    val fileName: String = "server.properties",
-) {
-    val values = mutableMapOf<String, String>()
-
+    fileName: String = "server.properties",
+    defaultResourceLocation: String = "/default/$fileName",
+    generateDefault: Boolean = true,
+) : FileConfiguration(fileName, defaultResourceLocation, generateDefault) {
     init {
         require(fileName.isNotBlank()) { "fileName cannot be blank" }
         require(fileName.endsWith(".properties")) { "fileName must end with .properties" }
     }
 
-    fun load() {
+    override fun save(): Unit = throw UnsupportedOperationException("Not implemented")
+
+    override fun generateMissing(missing: Configuration) {
         val file = File(fileName)
-        val input =
-            if (file.exists()) {
-                file.inputStream()
-            } else {
-                this.javaClass.getResourceAsStream("/$fileName")
-            }
-
-        input.use { inputStream ->
-            inputStream.bufferedReader().use { reader ->
-                reader.readLines().forEach { line ->
-                    if (line.startsWith("#") || line.isBlank()) {
-                        return@forEach
-                    }
-
-                    val parts = line.split("=")
-                    if (parts.size != 2) {
-                        return@forEach
-                    }
-
-                    val key = parts[0].trim()
-                    val value = parts[1].split("#")[0].trim()
-                    values[key] = value
-                }
-            }
-        }
-    }
-
-    fun generateMissing() {
-        val file = File(fileName)
-        if (!file.exists()) {
-            throw IllegalArgumentException("File $fileName does not exist")
-        }
-
-        val notExisting = mutableMapOf<String, String>()
-
-        this.javaClass.getResourceAsStream("/$fileName").use { inputStream ->
-            if (inputStream == null) {
-                throw IllegalArgumentException("Resource not found: $fileName")
-            }
-
-            inputStream.bufferedReader().readLines().forEach { line ->
-                if (!line.startsWith("#") && line.isNotBlank()) {
-                    val split = line.split("=")
-                    val key = split[0].trim()
-                    if (!values.containsKey(key)) {
-                        val default = split[1].split("#")[0].trim()
-                        values[key] = default
-                        notExisting[key] = default
-                    }
-                }
-            }
-        }
-
-        if (notExisting.isNotEmpty()) {
-            file.appendText("\n")
-            notExisting.forEach { (key, value) ->
+        val values = missing.values
+        if (values.isNotEmpty()) {
+            file.appendText("\n# Generated automatically\n")
+            values.forEach { (key, value) ->
+                set(key, value)
                 file.appendText("$key=$value\n")
             }
         }
     }
 
-    fun getString(key: String): String = values[key] ?: throw IllegalArgumentException("Key $key not found")
-
-    fun getInt(key: String): Int = getString(key).toIntOrNull() ?: throw IllegalArgumentException("Key $key is not an integer")
-
-    fun getBoolean(key: String): Boolean =
-        getString(key).toBooleanStrictOrNull() ?: throw IllegalArgumentException("Key $key is not a boolean")
-
-    fun saveDefault() {
-        this.javaClass.getResourceAsStream("/$fileName").use { inputStream ->
-            if (inputStream == null) {
-                throw IllegalArgumentException("Resource not found: $fileName")
-            }
-
-            val outputFile = File(fileName)
-            if (!outputFile.exists()) {
-                outputFile.createNewFile()
-                outputFile.outputStream().use { outputStream ->
-                    inputStream.copyTo(outputStream)
+    override fun getMap(input: InputStream): Configuration {
+        val values = mutableMapOf<String, Any>()
+        input.bufferedReader().use { reader ->
+            reader.readLines().forEach { line ->
+                if (line.startsWith("#") || line.isBlank()) {
+                    return@forEach
                 }
+
+                val parts = line.split("=")
+                if (parts.size != 2) {
+                    return@forEach
+                }
+
+                val key = parts[0].trim()
+                val value = parts[1].split("#")[0].trim()
+                values[key] = value
             }
         }
+        return ConfigurationSection(values)
     }
 
     fun getProperties(): Properties {
