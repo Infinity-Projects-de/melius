c1355f77ffefb0e02693943bdfddf308cf1a8b15
Chechu <contact@chechu.dev>
2024-10-21 16:18:41 +0200

    feat: create basic scoreboard manager

--- src/main/kotlin/de/infinityprojects/mcserver/server/ScoreboardManager.kt
+++ src/main/kotlin/de/infinityprojects/mcserver/server/ScoreboardManager.kt
@@ -1,4 +1,59 @@
 package de.infinityprojects.mcserver.server
 
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.logger.slf4j.ComponentLogger
+import net.minestom.server.entity.Player
+import net.minestom.server.scoreboard.Sidebar
+import org.yaml.snakeyaml.Yaml
+import java.io.File
+
 class ScoreboardManager {
+    val sidebars: HashMap<String, Sidebar> = hashMapOf()
+    val logger = ComponentLogger.logger("ScoreboardManager")
+
+
+    init {
+        val yaml = Yaml()
+        val file = File("scoreboard.yml")
+        val config = yaml.load(file.inputStream()) as Map<String, Any>
+
+        if (config.isNotEmpty()) {
+
+            config.entries.forEach { (key, value) ->
+                if (value is Map<*, *>) {
+                    if (value["enabled"] == true) {
+                        val lines = value["content"]?.let {
+                            if (it is List<*>) {
+                                it.filterIsInstance<String>()
+                            } else {
+                                listOf<String>()
+                            }
+                        } ?: listOf<String>()
+
+                        val title = value["title"] as String
+
+                        val sidebar = Sidebar(Component.text(title))
+                        var i = lines.size - 1
+                        lines.forEach {
+                            logger.error("$i: line_$i - $it")
+                            sidebar.createLine(
+                                Sidebar.ScoreboardLine(
+                                    "line_$i",
+                                    Component.text(it),
+                                    i,
+                                )
+                            )
+                            i--
+                        }
+
+                        sidebars[key] = sidebar
+                    }
+                }
+             }
+        }
+    }
+
+    fun sendScoreboard(player: Player) {
+        sidebars.values.firstOrNull()?.addViewer(player)
+    }
 }
