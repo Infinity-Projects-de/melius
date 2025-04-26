26551c605b96ba1865691bef5890fa51594f7623
Chechu <contact@chechu.dev>
2025-04-26 19:15:17 +0200

    feat: create cod

---
+++ src/main/kotlin/de/infinityprojects/mcserver/entity/animal/fish/Cod.kt
@@ -0,0 +1,16 @@
+package de.infinityprojects.mcserver.entity.animal.fish
+
+import net.minestom.server.entity.EntityType
+import net.minestom.server.sound.SoundEvent
+
+class Cod: EntityFishSchool(EntityType.COD) {
+    override fun deathSound(): SoundEvent? = SoundEvent.ENTITY_COD_DEATH
+
+    override fun hurtSound(): SoundEvent? = SoundEvent.ENTITY_COD_HURT
+
+    override fun playAmbientSound() {
+        playSoundEvent(SoundEvent.ENTITY_COD_AMBIENT, soundVolume(), soundPitch())
+    }
+
+    override fun getFlopSound(): SoundEvent = SoundEvent.ENTITY_COD_FLOP
+}
