13b1014e1f5af18830eb8ba4f96c913d8c5bf649
Chechu <contact@chechu.dev>
2025-05-05 13:24:33 +0200

    feat: add generic AbstractGolemMeta

--- src/main/kotlin/de/infinityprojects/mcserver/entity/golem/AbstractGolem.kt
+++ src/main/kotlin/de/infinityprojects/mcserver/entity/golem/AbstractGolem.kt
@@ -2,7 +2,8 @@ package de.infinityprojects.mcserver.entity.golem
 
 import de.infinityprojects.mcserver.entity.PathfinderMob
 import net.minestom.server.entity.EntityType
+import net.minestom.server.entity.metadata.golem.AbstractGolemMeta
 
-abstract class AbstractGolem(type: EntityType): PathfinderMob(type) {
+abstract class AbstractGolem<M : AbstractGolemMeta>(type: EntityType): PathfinderMob<M>(type) {
     final override fun setSuperclassAttributes() { }
 }
