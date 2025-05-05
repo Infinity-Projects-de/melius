e21499468479bb7af3ea0fa1cef599496db44237
Chechu <contact@chechu.dev>
2025-05-05 12:50:26 +0200

    feat: add canMateWith function

--- src/main/kotlin/de/infinityprojects/mcserver/entity/animal/Animal.kt
+++ src/main/kotlin/de/infinityprojects/mcserver/entity/animal/Animal.kt
@@ -1,15 +1,16 @@
 package de.infinityprojects.mcserver.entity.animal
 
-import de.infinityprojects.mcserver.entity.PathfinderMob
+import de.infinityprojects.mcserver.entity.AgeableEntity
 import net.minestom.server.entity.EntityType
-import net.minestom.server.entity.MetadataDef
 import net.minestom.server.entity.attribute.Attribute
+import net.minestom.server.entity.metadata.animal.AnimalMeta
 import net.minestom.server.item.ItemStack
 import net.minestom.server.network.packet.server.play.ParticlePacket
 import net.minestom.server.particle.Particle
+import kotlin.random.Random
 
-open class Animal(type: EntityType): PathfinderMob(type) {
-    var parent: Animal? = null
+open class Animal<M : AnimalMeta>(type: EntityType): AgeableEntity<M>(type) {
+    var parent: Animal<M>? = null
         private set(animal) {
             if (parent != null) {
                 throw IllegalStateException("Animal already has a parent")
@@ -18,35 +19,28 @@ open class Animal(type: EntityType): PathfinderMob(type) {
         }
 
     var inLove = 0
-    var age = 0
-        private set(value) {
-            if (value < 0) {
-                throw IllegalArgumentException("Age cannot be negative")
-            }
-            if (value < age) {
-                throw IllegalArgumentException("Age cannot be decreased")
-            }
-            age = value
-        }
+
+    val isInLove
+        get() = inLove > 0
 
     override fun aiTick(time: Long) {
         super.aiTick(time)
 
-        if (this.age != 0) {
-            this.inLove = 0
+        if (age != 0) {
+            inLove = 0
         }
 
-        if (this.inLove > 0) {
-            --this.inLove
-            if (this.inLove % 10 == 0) {
-                val oX = random.nextFloat() * 0.2f + 0.1f
-                val oY = random.nextFloat() * 0.2f + 0.1f
-                val oZ = random.nextFloat() * 0.2f + 0.1f
+        if (inLove > 0) {
+            --inLove
+            if (inLove % 10 == 0) {
+                val oX = Random.Default.nextFloat() * 0.2f + 0.1f
+                val oY = Random.Default.nextFloat() * 0.2f + 0.1f
+                val oZ = Random.Default.nextFloat() * 0.2f + 0.1f
 
                 val pos = position
-                val pX = pos.x + random.nextDouble() * 2.0 - 1.0
-                val pY = pos.y + random.nextDouble() * 2.0 - 1.0
-                val pZ = pos.z + random.nextDouble() * 2.0 - 1.0
+                val pX = pos.x + Random.Default.nextDouble() * 2.0 - 1.0
+                val pY = pos.y + Random.Default.nextDouble() * 2.0 - 1.0
+                val pZ = pos.z + Random.Default.nextDouble() * 2.0 - 1.0
 
                 val particle = ParticlePacket(Particle.HEART, pX, pY, pZ, oX, oY, oZ, 1.0f, 1)
                 instance.sendGroupedPacket(particle)
@@ -62,6 +56,23 @@ open class Animal(type: EntityType): PathfinderMob(type) {
         return false
     }
 
-    val isBaby: Boolean
-        get() = metadata[MetadataDef.AgeableMob.IS_BABY] ?: false
+    open fun canMateWith(other: Animal<out AnimalMeta>): Boolean {
+        if (this == other) {
+            return false
+        }
+
+        if (this.entityType != other.entityType) {
+            return false
+        }
+
+        if (other.isBaby || this.isBaby) {
+            return false
+        }
+
+        if (this.isInLava || other.isInLava) {
+            return false
+        }
+
+        return true
+    }
 }
