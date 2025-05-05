f367992a7f86be3b04084fd1af6f723de80f0bd9
Chechu <contact@chechu.dev>
2025-05-05 12:56:32 +0200

    feat: add variants

--- src/main/kotlin/de/infinityprojects/mcserver/entity/animal/Cow.kt
+++ src/main/kotlin/de/infinityprojects/mcserver/entity/animal/Cow.kt
@@ -1,7 +1,15 @@
 package de.infinityprojects.mcserver.entity.animal
 
+import net.minestom.server.component.DataComponents
 import net.minestom.server.entity.EntityType
+import net.minestom.server.entity.metadata.animal.CowMeta
+import net.minestom.server.entity.metadata.animal.CowVariant
+import net.minestom.server.registry.DynamicRegistry
 
-class Cow: AbstractCow(EntityType.COW){
-    // variants
+class Cow: AbstractCow<CowMeta>(EntityType.COW){
+    var variant: DynamicRegistry.Key<CowVariant>
+        get() = get(DataComponents.COW_VARIANT) ?: CowVariant.TEMPERATE
+        set(value) {
+            set(DataComponents.COW_VARIANT, value)
+        }
 }
