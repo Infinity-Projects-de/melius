e64f52d886ed12905839c3340ce8aa26dada75dc
Chechu <contact@chechu.dev>
2025-05-05 12:57:53 +0200

    feat: add generic type (out AgeableWaterAnimalMeta)

--- src/main/kotlin/de/infinityprojects/mcserver/entity/animal/water/AgeableWaterAnimal.kt
+++ src/main/kotlin/de/infinityprojects/mcserver/entity/animal/water/AgeableWaterAnimal.kt
@@ -1,8 +1,9 @@
 package de.infinityprojects.mcserver.entity.animal.water
 
-import de.infinityprojects.mcserver.entity.PathfinderMob
+import de.infinityprojects.mcserver.entity.AgeableEntity
 import net.minestom.server.entity.EntityType
+import net.minestom.server.entity.metadata.water.AgeableWaterAnimalMeta
 
-abstract class AgeableWaterAnimal(type: EntityType): PathfinderMob(type) {
+abstract class AgeableWaterAnimal<M : AgeableWaterAnimalMeta>(type: EntityType): AgeableEntity<M>(type) {
     override fun setSuperclassAttributes() {}
 }
